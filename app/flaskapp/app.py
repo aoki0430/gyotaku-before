1d8567e80e37232ff3ba78dcd269125f7c4c4684
aoki0430 <pankun0714@gmail.com>
2022-07-16 19:27:56 +0900

    first commit

---
+++ app/flaskapp/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+        return '<h2>Hello Flask+uWSGI+Nginx + hotreload</h2>'
+
+if __name__ == "__main__":
+	app.run(debug=True, host='0.0.0.0')
+else:
+    application = app
