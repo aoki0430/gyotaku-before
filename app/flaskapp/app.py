8eba2c9d8592c480fb691d306e8ce38493f67fc4
aoki0430 <pankun0714@gmail.com>
2022-07-16 22:54:53 +0900

    add html

--- app/flaskapp/app.py
+++ app/flaskapp/app.py
@@ -1,12 +1,45 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, abort
+# import base64
+# import tempfile
+# from PIL import Image
+# import io
+# import numpy as np
+# import os
+# from mlmodel.test import cycle_gan
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/", methods=["GET", "POST"])
 def index():
-        return '<h2>Hello Flask+uWSGI+Nginx + hotreload</h2>'
+        if request.method == "GET":
+                return render_template("index.html")
+#         if request.method == "POST":
+#                 return render_template("index.html")
+                        # # アプロードされたファイルをいったん保存する
+                        # f = request.files["file"]
+                        # folderpath = tempfile.mkdtemp()
+                        # filepath = os.path.join(folderpath,"a.png")
+                        # #filepath = "{}/".format(tempfile.gettempdir()) + datetime.now().strftime("%Y%m%d%H%M%S") + ".png"
+                        
+                        # f.save(filepath)
+                        # image = Image.open(filepath)
+                        
+                        # # 画像処理部分
+                        # image = np.asarray(image)
+                        # width = len(image[0])
+                        # height = len(image)      
+                        # filtered = Image.fromarray(cycle_gan(folderpath))
+                        # filtered = filtered.resize(size=(width,height), resample=Image.LANCZOS)
+                        
+                        # # base64でエンコード
+                        # buffer = io.BytesIO()
+                        # filtered.save(buffer, format="PNG")
+                        # img_string = base64.b64encode(buffer.getvalue()).decode().replace("'", "")
+                        
+                        # result = "image size {}×{}".format(width, height)
+                        # return render_template("index.html", filepath=filepath, result=result, img_data=img_string)
 
 if __name__ == "__main__":
-	app.run(debug=True, host='0.0.0.0')
+        app.run(debug=True, host='0.0.0.0')
 else:
-    application = app
+        application = app
