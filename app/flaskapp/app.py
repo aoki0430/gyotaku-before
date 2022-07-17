916f31e46ed7d9e69e4b9dcf91b897df7c507b72
aoki0430 <pankun0714@gmail.com>
2022-07-17 10:47:05 +0900

    ファイルアップロード＆表示ができるようにした

--- app/flaskapp/app.py
+++ app/flaskapp/app.py
@@ -1,4 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for, abort
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+import os
+import cv2
+import datetime
+import numpy as np
+
+
 # from pythoch
 # import base64
 # import tempfile
@@ -23,10 +29,31 @@ MODEL_FILE = 'horse2zebra.t7'
 #         plt.imshow(np.transpose(grdi_imgs_arr, (1, 2, 0)))
 #         plt.show()
 
-@app.route("/", methods=["GET", "POST"])
-def index():
-        if request.method == "GET":
-                return render_template("index.html")
+# @app.route("/", methods=["GET", "POST"])
+# def index():
+#         if request.method == "GET":
+#                 return render_template("index.html")
+        # if request.method == 'POST':
+        # # ファイルがなかった場合の処理
+        #         # if 'file' not in request.files:
+        #         #         flash('ファイルがありません')
+        #         #         return redirect(request.url)
+        #         # # データの取り出し
+        #         file = request.files['file']
+        #         # # ファイル名がなかった時の処理
+        #         # if file.filename == '':
+        #         #         flash('ファイルがありません')
+        #         #         return redirect(request.url)    
+        #         # # ファイルのチェック
+        #         # if file and allwed_file(file.filename):
+        #         #         # 危険な文字を削除（サニタイズ処理）
+        #         #         filename = secure_filename(file.filename)
+        #         filename = file.filename
+        #         # ファイルの保存
+        #         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        #         # アップロード後のページに転送
+        #         return redirect(url_for('uploaded_file', filename=filename))
+
 #         if request.method == "POST":
 #                 return render_template("index.html")
                         # # アプロードされたファイルをいったん保存する
@@ -52,6 +79,30 @@ def index():
                         
                         # result = "image size {}×{}".format(width, height)
                         # return render_template("index.html", filepath=filepath, result=result, img_data=img_string)
+
+# 画像のアップロード先のディレクトリ
+# UPLOAD_FOLDER = './uploads'
+# # アップロードされる拡張子の制限
+# ALLOWED_EXTENSIONS = set(['png', 'jpg', 'gif'])
+# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route("/", methods=["GET", "POST"])
+def uploaded_file():
+        img_dir = "static/imgs/"
+        if request.method == 'GET': img_path=None
+        elif request.method == 'POST':
+                #### POSTにより受け取った画像を読み込む
+                stream = request.files['img'].stream
+                img_array = np.asarray(bytearray(stream.read()), dtype=np.uint8)
+                img = cv2.imdecode(img_array, 1)
+                #### 現在時刻を名前として「imgs/」に保存する
+                dt_now = datetime.datetime.now().strftime("%Y%m%d%H%M%S%f")
+                img_path = img_dir + dt_now + ".jpg"
+                cv2.imwrite(img_path, img)
+                #### 保存した画像ファイルのpathをHTMLに渡す
+        # return render_template('index.html') 
+        return render_template('index.html', img_path=img_path) 
+
 @app.route("/image", methods=["GET"])
 def plotView():
         return render_template("image.html", success_message = '画像できたよ')
